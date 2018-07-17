52bb632054bf7ccd97e51509cb102b55156c56ac
nastone <nathanstone3000@gmail.com>
2018-07-17 14:10:41 -0600

    front end started

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Landing extends Component {
+    constructor(){
+        super();
+        this.state= {
+            pin: ''
+        }
+        this.handleInput = this.handleInput.bind(this);
+    }
+    handleInput(e){
+        this.setState({
+            pin: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    <input placeholder='Game PIN' onChange={this.handleInput} />
+                    <Link to={`/game/${this.state.pin}`}>
+                    <button>ENTER</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+}
