be533e78f91ba70fb2f2d4e5fbb6d995b330c8b5
Charles Hatch <charlesghatch@gmail.com>
2018-07-23 11:42:58 -0600

    attempting css-grid

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleNickname, selectedPin } from '../../Ducks/Reducer';
+import '../../App.css';
+import kwizz from '../../Assests/Kwizz.svg'
 
 class Landing extends Component {
     constructor() {
@@ -38,27 +40,46 @@ class Landing extends Component {
     }
     render() {
         return (
-            <div>
-                {
-                    !this.state.toggle
+            
+            <div className='background' >
+                <div className='container' >
+                    <div></div> 
+                    {
+                        !this.state.toggle
                         ?
-                        <div>
-                            <input type='number' value={this.state.pin} placeholder='Game PIN' onChange={this.handleInput} />
-                            <button onClick={this.handleToggle} >next</button>
-                        </div>
-                        :
-                        <div>
-                            <input type='text' value={this.state.nickname} placeholder='Nickname' onChange={this.handleNicknameInput} />
-                            <Link to='/player'>
-                                <button onClick={this.handleGo}>OK,go!</button>
-                            </Link>
-
-                        </div>
-                }
-                <div>
-                    <a href='http://localhost:3030/auth'>HOST</a>
+                        <div className='container' >
+                                <div></div> 
+                                <img src={kwizz} className=''/>
+                                <div></div> 
+                                <div></div> 
+                                <input type='number' value={this.state.pin} placeholder='Game PIN' onChange={this.handleInput} className='input-username ' />
+                                <div></div> 
+                                <div></div> 
+                                <button onClick={this.handleToggle} className='btn-enter ' >Enter</button>
+                                <div></div> 
+                            </div>
+                            :
+                            <div className='container' >
+                                <div></div> 
+                               <img src={kwizz} className=''/>
+                                <div></div> 
+                                <div></div> 
+                                <input type='text' value={this.state.nickname} placeholder='Nickname' onChange={this.handleNicknameInput} className='input-username  '/>
+                                <div></div> 
+                                <div></div> 
+                                <Link to='/player'>
+                                    <button onClick={this.handleGo} className='btn-enter '>OK,go!</button>
+                                </Link>
+                                <div></div> 
+                            </div>
+                    }
+                    <div></div> 
+                    <div></div>
+                        <a href='http://localhost:3030/auth' className='btn-enter' >HOST</a>
+                    <div></div> 
+                   
                 </div>
-            </div>
+             </div> 
         )
     }
 }
