1644c99b5e3d856284114c5fb61e44a06f6c6499
Dylan Lovell <Dylovell@live.com>
2018-07-26 16:39:06 -0600

    added somewhat functioning timer

--- src/components/Game/Game_Questions.js
+++ src/components/Game/Game_Questions.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function GameQuestions(props){
     return(
         <div>
+            <h1>there is only {props.timer} seconds left!</h1>
             <h1>{props.question}</h1>
             <div><p>{props.answer1}</p></div> 
             <div><p>{props.answer2}</p></div>
