b1adf3935fa0cf9460d98d84ccdf353b163be865
Dylan Lovell <Dylovell@live.com>
2018-07-30 09:50:35 -0600

    Added last place to leader board

--- src/components/Game/Game_Over.js
+++ src/components/Game/Game_Over.js
@@ -7,7 +7,7 @@ export default function GameOver(props){
             <h1>Game Over</h1>
             <p>1st Place: {props.leaderboard[0].name}</p>
             <p>2nd Place: {props.leaderboard[1].name}</p>
-            <p>3rd Place: {props.leaderboard[2].name}</p>
+            <p>Last Place: {props.leaderboard[props.leaderboard.length()-1].name}</p>
             <Link to='/host'>Start a new Game?</Link>
         </div> 
     )
